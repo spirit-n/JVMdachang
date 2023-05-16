400fc2744cfc9e1bdac8a1ded9329617e6e55d55
刘彦斌 <2554532680@qq.com>
2023-05-16 22:10:55 +0800

    runFinalization

--- chapter06_gc/src/com/atguigu/other/SystemGCTest.java
+++ chapter06_gc/src/com/atguigu/other/SystemGCTest.java
@@ -10,7 +10,7 @@ public class SystemGCTest {
         System.gc();//提醒jvm的垃圾回收器执行full gc,但是不确定是否马上执行gc
 //        与Runtime.getRuntime().gc();的作用一样。
 
-//        System.runFinalization();//强制调用使用引用的对象的finalize()方法
+       System.runFinalization();//强制调用使用引用的对象的finalize()方法
     }
 
     //此方法什么时候会被执行？ 当一个对象首次考虑要被回收时，会调用其finalize()
