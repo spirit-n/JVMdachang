5ac5d219905f92449611529334e45f70d7b46f22
刘彦斌 <2554532680@qq.com>
2023-05-23 22:24:00 +0800

    log

--- chapter06_gc/src/com/atguigu/other/SystemGCTest.java
+++ chapter06_gc/src/com/atguigu/other/SystemGCTest.java
@@ -7,7 +7,7 @@ package com.atguigu.other;
 public class SystemGCTest {
     public static void main(String[] args) {
         new SystemGCTest();
-        System.gc();//提醒jvm的垃圾回收器执行full gc,但是不确定是否马上执行gc
+        // System.gc();//提醒jvm的垃圾回收器执行full gc,但是不确定是否马上执行gc
        // 与Runtime.getRuntime().gc();的作用一样。
 
        System.runFinalization();//强制调用使用引用的对象的finalize()方法
