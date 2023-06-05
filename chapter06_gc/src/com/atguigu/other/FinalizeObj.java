5e0f2b809c048639607b714efd1fa6c28c65c269
刘彦斌 <2554532680@qq.com>
2023-06-05 22:11:54 +0800

    log

--- chapter06_gc/src/com/atguigu/other/FinalizeObj.java
+++ chapter06_gc/src/com/atguigu/other/FinalizeObj.java
@@ -4,7 +4,6 @@ package com.atguigu.other;
  * 测试Object类中finalize()方法，即对象的finalization机制。
  *
  * @author shkstart
- * @create
  */
 public class FinalizeObj {
     public static FinalizeObj obj;//类变量，属于 GC Root
